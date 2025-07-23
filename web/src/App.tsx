a05f626cb88752d0882b59d982a3ac1fc0e6619a
alexiao94 <68236804+alexiao94@users.noreply.github.com>
2025-07-22 20:56:24 -0500

    AY-42-Dynamic Datepicker

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./components/Navbar";
+import DatePicker from "./components/Datepicker";
+import * as React from "react";
 
 interface Link {
   url: string;
@@ -15,9 +17,15 @@ function App() {
     { url: "/portal", label: "Portal" },
     { url: "/account", label: "Account Info" },
   ];
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const occupied: Date[] = [];
   return (
     <div>
-      <Navbar links={links} isLogged={false} />
+      <DatePicker
+        selectedDate={date}
+        setSelectedDate={setDate}
+        occupiedDates={occupied}
+      />
     </div>
   );
 }
