5506d3ab291d994f69fb6f755a969cf68978d715
alexiao94 <68236804+alexiao94@users.noreply.github.com>
2025-07-30 12:20:42 -0500

    AY-52-Change grid layout to carousel

--- web/src/components/ServiceCard.tsx
+++ web/src/components/ServiceCard.tsx
@@ -20,7 +20,7 @@ interface ServiceProps {
 const ServiceCard = ({ service, button_action, card_action_click }: ServiceProps) => {
 
   return (
-    <Card className="w-full max-w-sm shadow-lg hover:shadow-2xl hover:shadow-blue-400 transition-shadow duration-300" onClick={card_action_click}>
+    <Card className="w-full h-full max-w-xs shadow-lg hover:shadow-2xl hover:shadow-blue-400 transition-shadow duration-300" onClick={card_action_click}>
       <CardHeader>
         <CardTitle>
           <div className="flex justify-between">
@@ -34,7 +34,7 @@ const ServiceCard = ({ service, button_action, card_action_click }: ServiceProps
           className="flex w-full h-full object-contain flex-shrink-0 rounded-md"
         />
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex aspect-square items-center justify-center p-6">
         <CardDescription>{service.description}</CardDescription>
       </CardContent>
       <CardFooter className="flex w-full justify-end">
