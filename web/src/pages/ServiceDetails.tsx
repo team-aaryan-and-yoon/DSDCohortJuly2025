29d4cb3d7959823bd70ed6ddc9153e91a420ad57
alexiao94 <68236804+alexiao94@users.noreply.github.com>
2025-07-30 08:06:26 -0500

    AY-52- Added some comments

--- web/src/pages/ServiceDetails.tsx
+++ web/src/pages/ServiceDetails.tsx
@@ -4,13 +4,16 @@ import { serviceData } from "@/examples/data";
 import type { serviceType } from "@/Types";
 import { useEffect, useState } from "react";
 
-// TODO:
-// we need to decide how to handle reviews and details data retrieval
+/* TODO:
+ - we need to decide how to handle reviews and details data retrieval
+ - add the fetching mechanism/logic
+ - improve UI/UX 
+*/
 const ServiceDetailsPage = () => {
   const [services, setServices] = useState<serviceType[]>(serviceData); 
   const [selectedService, setSelectedService] = useState<serviceType>();
 
-  // use this to update the selected service details and reviews
+  // use this to update the selected service details and reviews through axios or fetch
   useEffect(()=> {
 
   }, []) 
@@ -24,7 +27,7 @@ const ServiceDetailsPage = () => {
           <ServiceCard
             key={key}
             service={service}
-            button_action={() => console.log("Redirect to booking")}
+            button_action={() => console.log("Redirect to booking")} // Need to update to fit purpose
             card_action_click={() => setSelectedService(service)}
           />
           
@@ -34,7 +37,8 @@ const ServiceDetailsPage = () => {
       <div className="flex flex-col flex-1 w-1/2 h-full justify-center gap-2">
         <div className="w-full h-full border-2 rounded-md">
           <div className="flex flex-col w-full h-full justify-center items-center">
-            <div className="flex w-full h-full justify-center items-center">{selectedService?.details}</div>
+            {/* Need to update to fit purpose */}
+            <div className="flex w-full h-full justify-center items-center">{selectedService?.details } </div>
             <div className="flex w-full h-full justify-end items-end p-4">
               <Button>
                 Book now
@@ -42,6 +46,7 @@ const ServiceDetailsPage = () => {
               </div>
           </div>
         </div>
+        {/*Need to create reviews component*/}
         <div className="w-full h-full border-2 rounded-md">Service Reviews</div>
       </div>
     </div>
