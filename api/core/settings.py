ecdee8281e6b029ab45b005866aec2b179f956c2
aluko123 <oaa0008@uah.edu>
2025-08-10 21:12:53 -0500

    Updated email update endpoint. Need to test

--- api/core/settings.py
+++ api/core/settings.py
@@ -194,3 +194,11 @@ SPECTACULAR_SETTINGS = {
         }
     },
 }
+
+#Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast = int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
